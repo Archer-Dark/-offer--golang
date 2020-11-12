90ad4807561916feb9ce62cbb518104e9e3fde6f
fan-shangshang <fsjyh1988@163.com>
2020-11-12 19:44:26 +0800

    新增旋转数组最小数字解法

--- 010-旋转数组的最小数字/problem010.go
+++ 010-旋转数组的最小数字/problem010.go
@@ -20,19 +20,34 @@ import "fmt"
 //     return numbers[0]
 // }
 
-func minArray(numbers []int) int {
-	l,r := 0,len(numbers)-1
-	for l<r{
-		m := l+((r-l)>>1)
-		if numbers[m] > numbers[r]{
-			l = m+1
-		}else if numbers[m] < numbers[r] {
-			r = m
-		}else {
-			r--
+//func minArray(numbers []int) int {
+//	l,r := 0,len(numbers)-1
+//	for l<r{
+//		m := l+((r-l)>>1)
+//		if numbers[m] > numbers[r]{
+//			l = m+1
+//		}else if numbers[m] < numbers[r] {
+//			r = m
+//		}else {
+//			r--
+//		}
+//	}
+//	return numbers[l]
+//}
+
+func minArray(rotateArray []int) int{
+	i,j := 0,len(rotateArray)-1
+	for i < j {
+		if rotateArray[i] > rotateArray[i+1]{
+			return rotateArray[i+1]
 		}
+		if rotateArray[j] < rotateArray[j-1]{
+			return rotateArray[j]
+		}
+		i++
+		j--
 	}
-	return numbers[l]
+	return rotateArray[0]
 }
 
 func main()  {
