fbf59e612d1e1d42103651d4c3252a74a3aebcec
fan-shangshang <fsjyh1988@163.com>
2020-12-17 11:24:32 +0800

    调整byte数组0下表赋值时间

--- 018-两大数相加/problem018.go
+++ 018-两大数相加/problem018.go
@@ -9,14 +9,14 @@ func mySum(x,y string) string {
 	if len(x) < len(y) {
 		x,y = y,x
 	}
-	difference := len(x) - len(y)
+	diff := len(x) - len(y)
 	carry := 0
 	numBytes := make([]uint8,len(x)+1)
 	for i := len(x)-1;i>=0;i-- {
 		a,_ := strconv.Atoi(x[i:i+1])
 		b := 0
-		if i >= difference{
-			b,_ = strconv.Atoi(y[i-difference:i+1-difference])
+		if i >= diff{
+			b,_ = strconv.Atoi(y[i-diff:i+1-diff])
 		}
 		c := a + b + carry
 		if c >= 10 {
@@ -28,11 +28,11 @@ func mySum(x,y string) string {
 		num := strconv.Itoa(c)[0]
 		numBytes[i+1] = num
 	}
-	numBytes[0] = uint8('1')
 	if carry == 0 {
 		numBytes = numBytes[1:]
 		return *(*string)(unsafe.Pointer(&numBytes))
 	}
+	numBytes[0] = uint8('1')
 	return *(*string)(unsafe.Pointer(&numBytes))
 }
 
